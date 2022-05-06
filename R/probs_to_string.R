cf57e3de3ec2a97a275e6d082dbf93d0e0d43170
Daniel J. McDonald <dajmcdon@gmail.com>
2022-05-05 18:44:04 -0700

    fix digits issue (commented out in get_precision()) revisit later

--- R/probs_to_string.R
+++ R/probs_to_string.R
@@ -12,29 +12,29 @@
 
 #' @importFrom stats setNames
 #' @export
-get_precision <- function(x, ...){
-    # from [Broman::get_precision()]
-    # a bit of contortion here to control the scipen and digits options and have them returned to their initial values
-    dots <- list("...")
-    if(is.null(dots$set_digits) || dots$set_digits) {
-      scipen <- options("scipen")$scipen
-      digits <- options("digits")$digits
-      on.exit(options(scipen=scipen, digits=digits))
-      options(scipen=100, digits=21)
-    }
+get_precision <- function(x, ...) {
+  # from [Broman::get_precision()]
+  # a bit of contortion here to control the scipen and digits options and have them returned to their initial values
+  dots <- list("...")
+  # if (is.null(dots$set_digits) || dots$set_digits) {
+  #   scipen <- options("scipen")$scipen
+  #   digits <- options("digits")$digits
+  #   on.exit(options(scipen=scipen, digits=digits))
+  #   options(scipen=1, digits=8)
+  # }
 
-    if(length(x) > 1) { # deal with vector input
-      return(setNames(vapply(x, get_precision, 1, set_digits=FALSE), NULL))
-    }
+  if (length(x) > 1) { # deal with vector input
+    return(setNames(vapply(x, get_precision, 1, set_digits=FALSE), NULL))
+  }
 
-    ### here down, x is a single value
-    if(is.na(x)) return(NA) # NA -> NA
-    x <- as.character(x)
-    if(!grepl(".", x, fixed=TRUE)) return(0)
-    frac <- strsplit(x, ".", fixed=TRUE)[[1]][2]
-    if(is.na(frac) || nchar(frac)==0) return(0)
-    digits <- strsplit(frac, "", fixed=TRUE)[[1]]
-    max(which(digits != "0"))
+  ### here down, x is a single value
+  if(is.na(x)) return(NA) # NA -> NA
+  x <- as.character(x)
+  if(!grepl(".", x, fixed=TRUE)) return(0)
+  frac <- strsplit(x, ".", fixed=TRUE)[[1]][2]
+  if(is.na(frac) || nchar(frac)==0) return(0)
+  digits <- strsplit(frac, "", fixed=TRUE)[[1]]
+  max(which(digits != "0"))
 }
 
 probs_to_string <- function(x, prefix = "q") {
