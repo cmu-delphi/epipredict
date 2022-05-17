e4d1e591ea90e4ad6b01dfe4ac04bfc19b86788b
Daniel J. McDonald <daniel@stat.ubc.ca>
2022-05-17 12:12:06 -0700

    epi_recipe testing stub

--- tests/testthat/test-epi_recipe.R
+++ tests/testthat/test-epi_recipe.R
@@ -1,3 +1,11 @@
-test_that("epi_recipe produces the correct structure", {
 
+
+test_that("epi_recipe produces default recipe", {
+  tib <- tibble(
+    x = 1:5, y = 1:5,
+    time_value = seq(as.Date("2020-01-01"), by = 1, length.out = 5)
+  )
+  expect_identical(recipes::recipe(tib), epi_recipe(tib))
+  expect_identical(recipes::recipe(y ~ x, tib),
+                   epi_recipe(y ~ x, tib))
 })
