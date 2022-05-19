86fa4bb7a465e2f3ee47fb00d7a27aa6fafec63e
Daniel J. McDonald <daniel@stat.ubc.ca>
2022-05-19 10:54:47 -0700

    poorly constructed test

--- tests/testthat/test-epi_shift.R
+++ tests/testthat/test-epi_shift.R
@@ -21,7 +21,8 @@ test_that("epi shift works with groups", {
 test_that("epi shift single works, renames", {
   tib <- tibble(
     x = 1:5, y = 1:5,
-    time_value = seq(as.Date("2020-01-01"), by = 1, length.out = 5)
+    time_value = seq(as.Date("2020-01-01"), by = 1, length.out = 5),
+    geo_value = "ca"
   ) %>% epiprocess::as_epi_df()
   ess <- epi_shift_single(tib, "x", 1, "test", epi_keys(tib))
   expect_named(ess, c("time_value", "geo_value", "test"))
