f5ab89087775cd7deabe6ff614bdf3bf94007097
kenmawer <kenmawer93@gmail.com>
2022-05-25 09:30:24 -0700

    Updated test

--- tests/testthat/test-assign_arg_list.R
+++ tests/testthat/test-assign_arg_list.R
@@ -1,3 +1,6 @@
-test_that("Stop when errors occur",{
+test_that("First argument must be a list",{
   expect_error(assign_arg_list(c(1,2,3)))
+})
+test_that("All arguments should be named",{
+  expect_error(assign_arg_list(list(1,2)))
 })
