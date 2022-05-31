d9a41547a0a665fd43980d41fe1592703970b3f0
admin <admin@host70-185.resnet.ubc.ca>
2022-05-31 02:08:31 -0700

    Added name for predictions column

--- R/epidf_predict.R
+++ R/epidf_predict.R
@@ -26,7 +26,7 @@ epidf_predict <- function(obj, new_data, ahead, forecast_date = NULL) {
     keys_df_fcd <- keys_df %>%
       dplyr::mutate(time_value = as.Date(forecast_date))
 
-    pred_df <- as_epi_df(dplyr::bind_cols(keys_df_fcd, pred_df),
+    pred_df <- as_epi_df(dplyr::bind_cols(keys_df_fcd, pred = pred_df),
                          geo_type = attributes(new_data)$metadata$geo_type,
                          time_type = attributes(new_data)$metadata$time_type,
                          as_of = attributes(new_data)$metadata$as_of)
