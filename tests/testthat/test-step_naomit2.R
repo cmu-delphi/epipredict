6b7862f453a7f78878c07eb72a88823d6a073155
kenmawer <kenmawer93@gmail.com>
2022-06-03 12:15:55 -0700

    Polished test.

--- tests/testthat/test-step_naomit2.R
+++ tests/testthat/test-step_naomit2.R
@@ -11,20 +11,17 @@ x <- tibble(geo_value = rep("nowhere",200),
             death_rate = rpois(100,10) + 1:200) %>%
   as_epi_df()
 
+# Preparing the datasets to be used for comparison
 r <- epi_recipe(x) %>%
   step_epi_ahead(death_rate, ahead = 7) %>%
   step_epi_lag(death_rate, lag = c(0,7,14))
-
-extract <- function(recipe) {
-  recipe
-}
 
 z1 <- step_naomit2(r)
 z2 <- r %>%
   step_naomit(all_predictors()) %>%
   step_naomit(all_outcomes(), skip = TRUE)
 
-# Tests
+# Test
 test_that("Check that both functions behave the same way", {
   expect_identical(z1$steps[[3]][-1][-5],z2$steps[[3]][-1][-5])
   expect_identical(z1$steps[[4]][-1][-5],z2$steps[[4]][-1][-5])
