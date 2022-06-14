c092f0e65e77bd2211f00c8c995b395dff9011b3
kenmawer <kenmawer93@gmail.com>
2022-06-14 12:51:38 -0700

    Still needs to pass checks.

--- tests/testthat/test-epi_keys.R
+++ tests/testthat/test-epi_keys.R
@@ -1,3 +1,5 @@
+library(tidymodels)
+
 test_that("epi_keys returns null for an object that isn't an epi_df",{
   expect_null(epi_keys(data.frame(x=1:3,y=2:4)))
 })
@@ -11,7 +13,7 @@ test_that("Extracts keys from a recipe; roles are NA, giving an empty vector",{
   expect_equal(epi_keys(recipe(case_death_rate_subset)),c("")[-1])
 })
 
-test_that("mold behaves properly",{
+test_that("epi_keys_mold extracts time_value and geo_value, but not raw",{
   my_recipe <- epi_recipe(case_death_rate_subset) %>%
     step_epi_ahead(death_rate, ahead = 7) %>%
     step_epi_lag(death_rate, lag = c(0,7,14)) %>%
@@ -23,6 +25,6 @@ test_that("mold behaves properly",{
     add_model(linear_reg()) %>%
     fit(data = case_death_rate_subset)
 
-  expect_equal(mold(my_workflow$pre$mold),
+  expect_equal(epi_keys_mold(my_workflow$pre$mold),
                c("time_value","geo_value"))
 })
