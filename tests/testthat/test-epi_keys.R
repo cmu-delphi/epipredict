70cf350be7dc2893c9e5adcbfa762afe4ecdcac8
kenmawer <kenmawer93@gmail.com>
2022-06-14 12:34:25 -0700

    Added a test.

---
+++ tests/testthat/test-epi_keys.R
@@ -0,0 +1,28 @@
+test_that("epi_keys returns null for an object that isn't an epi_df",{
+  expect_null(epi_keys(data.frame(x=1:3,y=2:4)))
+})
+
+test_that("Extracts keys from an epi_df",{
+  expect_equal(epi_keys(case_death_rate_subset),
+                c("time_value","geo_value"))
+})
+
+test_that("Extracts keys from a recipe; roles are NA, giving an empty vector",{
+  expect_equal(epi_keys(recipe(case_death_rate_subset)),c("")[-1])
+})
+
+test_that("mold behaves properly",{
+  my_recipe <- epi_recipe(case_death_rate_subset) %>%
+    step_epi_ahead(death_rate, ahead = 7) %>%
+    step_epi_lag(death_rate, lag = c(0,7,14)) %>%
+    step_epi_lag(case_rate, lag = c(0,7,14)) %>%
+    step_epi_naomit()
+
+  my_workflow <- workflow() %>%
+    add_recipe(my_recipe) %>%
+    add_model(linear_reg()) %>%
+    fit(data = case_death_rate_subset)
+
+  expect_equal(mold(my_workflow$pre$mold),
+               c("time_value","geo_value"))
+})
