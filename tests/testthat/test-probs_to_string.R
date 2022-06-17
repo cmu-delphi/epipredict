ea67dfc341c46659ae1fa2e6fb7d2e0ae27f9f05
kenmawer <kenmawer93@gmail.com>
2022-06-17 10:59:17 -0700

    Finished tests.

--- tests/testthat/test-probs_to_string.R
+++ tests/testthat/test-probs_to_string.R
@@ -1,3 +1,16 @@
-test_that("asdf",{
+test_that("tests get_precision",{
+  expect_equal(get_precision(2),0)
+  expect_equal(get_precision(c(3.0,3.12,3.003)),c(0,2,3))
+  expect_identical(get_precision(NA),NA)
+})
+
+test_that("probs_to_string throws errors when it should",{
+  expect_error(probs_to_string(100))
+  expect_error(probs_to_string(0.5,c("a","b")))
+  expect_null(probs_to_string(NULL))
+  expect_equal(probs_to_string(c(0.2),"abc"),"abc0.2")
+})
+
+test_that("x",{
 
 })
