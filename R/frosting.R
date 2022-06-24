391942bd63294b1b423e6210848a57d80b4fab92
admin <admin@host70-185.resnet.ubc.ca>
2022-06-24 00:09:20 -0700

    library(recipes)

--- R/frosting.R
+++ R/frosting.R
@@ -113,13 +113,16 @@ new_frosting <- function() {
 #' @export
 #'
 #' @examples
+#' library(dplyr)
+#' library(recipes)
+#'
 #' # Toy example to show that frosting can be created and added for postprocessing
 #'  f <- frosting()
 #'  wf <- epi_workflow() %>% add_frosting(f)
 #'
 #' # A more realistic example
 #' jhu <- case_death_rate_subset %>%
-#'   dplyr::filter(time_value > "2021-11-01", geo_value %in% c("ak", "ca", "ny"))
+#'   filter(time_value > "2021-11-01", geo_value %in% c("ak", "ca", "ny"))
 #'
 #' r <- epi_recipe(jhu) %>%
 #'   step_epi_lag(death_rate, lag = c(0, 7, 14)) %>%
@@ -127,7 +130,7 @@ new_frosting <- function() {
 #'   step_naomit(all_predictors()) %>%
 #'   step_naomit(all_outcomes(), skip = TRUE)
 #'
-#' wf <- epi_workflow(r, parsnip::linear_reg()) %>% fit(jhu)
+#' wf <- epi_workflow(r, parsnip::linear_reg()) %>% parsnip::fit(jhu)
 #' latest <- get_test_data(recipe = r, x = jhu)
 #'
 #' f <- frosting() %>%
