2792bc498915a6b693abef616482ac25de619539
admin <admin@host70-185.resnet.ubc.ca>
2022-07-07 18:11:51 -0700

    Enabled user to specify a target date

--- R/layer_add_target_date.R
+++ R/layer_add_target_date.R
@@ -4,7 +4,7 @@
 #' @param target_date The target date to add as a column to the `epi_df`.
 #' By default, this is `time_value` plus `ahead`, where `ahead` has
 #' been specified in preprocessing (most likely in `step_epi_ahead`).
-#' The user may override this with a date in the form "yyyy-mm-dd".
+#' The user may override this with a date usually of the form "yyyy-mm-dd".
 #' @param id a random id string
 #'
 #' @return an updated `frosting` postprocessor
@@ -26,12 +26,21 @@
 #' latest <- jhu %>%
 #'   dplyr::filter(time_value >= max(time_value) - 14)
 #'
+#' # Use ahead from preprocessing
 #' f <- frosting() %>% layer_predict() %>%
 #'   layer_add_target_date() %>% layer_naomit(.pred)
 #' wf1 <- wf %>% add_frosting(f)
 #'
 #' p <- predict(wf1, latest)
 #' p
+#'
+#' # Override default behaviour by specifying own target date
+#' f2 <- frosting() %>% layer_predict() %>%
+#' layer_add_target_date(target_date = "2022-01-08") %>% layer_naomit(.pred)
+#' wf2 <- wf %>% add_frosting(f2)
+#'
+#' p2 <- predict(wf2, latest)
+#' p2
 layer_add_target_date <-
   function(frosting, target_date = NULL, id = rand_id("add_target_date")) {
     add_layer(
@@ -58,7 +67,7 @@ slather.layer_add_target_date <- function(object, components, the_fit, the_recip
                                              target_date = ahead + components$predictions$time_value)
   } else{
     components$predictions <- dplyr::bind_cols(components$predictions,
-                                               target_date = object$target_date)
+                                               target_date = as.Date(object$target_date))
   }
   components
 }
