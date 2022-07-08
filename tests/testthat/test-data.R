6ba3d9fb71a80df1e72049f919baaf7a8de1edaf
kenmawer <kenmawer93@gmail.com>
2022-07-08 12:04:50 -0700

    Added a few more tests; still not finished.

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,3 +1,6 @@
 test_that("Data",{
   expect_equal(nrow(case_death_rate_subset),20496)
+  expect_equal(as.Date("2020-12-31"),min(case_death_rate_subset$time_value))
+  expect_equal(as.Date("2021-12-31"),max(case_death_rate_subset$time_value))
 })
+
