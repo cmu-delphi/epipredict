1a740212c6bab9f03181c14be019bd7e4239385c
rachlobay <rachel.lobay@gmail.com>
2022-08-11 20:29:08 -0700

    utils::

--- R/zzz.R
+++ R/zzz.R
@@ -4,9 +4,11 @@
 # environment inside of parsnip so they have to be executed once parsnip has
 # been loaded.
 
+# Also create an enviro. for epi_juice that is the same as for juice (so
+# can access the funs in recipes) & also make sure the funs in recipes use epi_juice
 .onLoad <- function(libname, pkgname) {
   make_flatline_reg()
 
   environment(epi_juice) <<- asNamespace('recipes')
-  assignInNamespace("juice", epi_juice, ns = "recipes")
+  utils::assignInNamespace("juice", epi_juice, ns = 'recipes')
 }
