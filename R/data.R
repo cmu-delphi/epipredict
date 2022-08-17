3864d7df106409f9154d1ea6b8f0d6baaaff63b4
Maggie Liu <mgyliu@gmail.com>
2022-08-16 22:17:42 -0700

    use a better dataset

--- R/data.R
+++ R/data.R
@@ -33,3 +33,52 @@
 #'   June 7 is the average of the underlying data for June 1 through 7,
 #'   inclusive.
 "case_death_rate_subset"
+
+#' Subset of Statistics Canada employment numbers by industry and province
+#' 
+#' @format A tibble with 109,388 rows and 6 variables:
+#' \describe{
+#'   \item{geo_value}{The province in Canada associated with each 
+#'      row of measurements.}
+#'   \item{time_value}{The time value, in YYYY-MM-01 format, 
+#'      associated with each row of measurements.}
+#'   \item{ppl_count}{The number of people employed, seasonally 
+#'      adjusted.}
+#'   \item{employee_type}{The type of employee}
+#'   \item{naics_industry}{The industry name and associated code 
+#'      according to \href{https://www23.statcan.gc.ca/imdb/p3VD.pl?Function=getVD&TVD=1181553}{NAICS}}
+#' }
+#' @source This object contains modified data from the following Statistics Canada
+#' data table: \href{https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=1410022001#data}{Table 14-10-0220-01  Employment and average weekly earnings (including overtime) for all employees by industry, monthly, seasonally adjusted, Canada}
+#' 
+#' Modifications:
+#' * From the given Statistics Canada table, the employee counts
+#'   are taken as-is. Only \href{https://www23.statcan.gc.ca/imdb/p3VD.pl?Function=getVD&TVD=1181553}{NAICS} codes at hierarchy level 2 are 
+#'   filtered in. Only data rows that are \href{https://www.statcan.gc.ca/en/concepts/definitions/guide-symbol}{good quality or higher and not missing}
+#'   according to Statistics Canada are removed.
+"statcan_employ_subset"
+
+#' Subset of Statistics Canada median employment income for postsecondary graduates
+#' 
+#' @format A tibble with 10193 rows and 8 variables:
+#' \describe{
+#'   \item{geo_value}{The province in Canada associated with each 
+#'      row of measurements.}
+#'   \item{time_value}{The time value, a year integer in YYYY format}
+#'   \item{edu_qual}{The education qualification}
+#'   \item{fos}{The field of study}
+#'   \item{age_group}{The age group; either 15 to 34 or 35 to 64}
+#'   \item{num_graduates}{The number of graduates for the given row of characteristics}
+#'   \item{med_income_2y}{The median employment income two years after graduation}
+#'   \item{med_income_5y}{The median employment income five years after graduation}
+#' }
+#' @source This object contains modified data from the following Statistics Canada 
+#' data table: \href{https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=3710011501}
+#' 
+#' Modifications:
+#' * Only provincial-level geo_values are kept
+#' * Only age group, field of study, and educational qualification are kept as 
+#'   covariates. For the remaining covariates, we keep aggregated values and 
+#'   drop the level-specific rows.
+#' * No modifications were made to the time range of the data
+"grad_employ_subset"
