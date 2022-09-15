8d8d5280ce4b77f57d6dae0c90958fe75b050950
Maggie Liu <mgyliu@gmail.com>
2022-09-15 02:06:46 -0700

    remove raw data generation eda lines

--- data-raw/grad_employ_subset.R
+++ data-raw/grad_employ_subset.R
@@ -91,11 +91,6 @@ gemploy <- statcan_grad_employ %>%
     !is.na(num_graduates) & !is.na(med_income_2y) & !is.na(med_income_5y)) %>%
   select(-c(status, gender, student_status, grad_charac, fos))
 
-# gemploy$time_value %>% unique()
-# class(gemploy$fos)
-# class(gemploy$edu_qual)
-# class(gemploy$age_group)
-# head(gemploy)
 nrow(gemploy)
 ncol(gemploy)
 
@@ -107,94 +102,3 @@ grad_employ_subset <- gemploy %>%
     geo_type = "custom", time_type = "year", as_of = "2022-07-19",
     additional_metadata = list(other_keys = c("age_group", "edu_qual")))
 usethis::use_data(grad_employ_subset, overwrite = TRUE)
-
-# ================== EDA ==================
-
-statcan_grad_employ %>% 
-  group_by(`Characteristics after graduation`) %>% 
-  # filter(`Graduate statistics` == "Median employment income two years after graduation") %>%
-  filter(`Graduate statistics` == "Number of graduates") %>%
-  slice(1) %>% 
-  select(`Characteristics after graduation`)
-
-names(statcan_grad_employ)
-unique(statcan_grad_employ$REF_DATE)
-# [1] "2010" "2011" "2012" "2013" "2014" "2015"
-
-unique(statcan_grad_employ$GEO)
-#  [1] "Canada"                    "Newfoundland and Labrador"
-#  [3] "Prince Edward Island"      "Nova Scotia"              
-#  [5] "New Brunswick"             "Quebec"                   
-#  [7] "Ontario"                   "Manitoba"                 
-#  [9] "Saskatchewan"              "Alberta"                  
-# [11] "British Columbia"          "Territories"  
-unique(statcan_grad_employ$DGUID)
-
-unique(statcan_grad_employ$UOM)
-# [1] "Number"                "2020 constant dollars"
-statcan_grad_employ %>% 
-  group_by(UOM, `Graduate statistics`) %>% 
-  select(UOM, `Graduate statistics`, VALUE) %>% 
-  slice(1)
-unique(statcan_grad_employ$UOM_ID)
-
-unique(statcan_grad_employ$SCALAR_FACTOR) # "units"
-unique(statcan_grad_employ$SCALAR_ID) # "0"
-
-length(unique(statcan_grad_employ$VECTOR))
-length(unique(statcan_grad_employ$COORDINATE))
-
-length(unique(statcan_grad_employ$VALUE))
-unique(statcan_grad_employ$STATUS)
-# [1] NA   ".." "x" 
-
-unique(statcan_grad_employ$SYMBOL)
-# [1] NA
-
-unique(statcan_grad_employ$TERMINATED)
-# [1] NA
-
-unique(statcan_grad_employ$DECIMALS)
-# [1] "0"
-
-unique(statcan_grad_employ$GeoUID)
-# redundant with geo
-
-unique(statcan_grad_employ$`Hierarchy for GEO`)
-# redundant with geo
-
-unique(statcan_grad_employ$`Classification Code for Educational qualification`) # NA
-unique(statcan_grad_employ$`Hierarchy for Educational qualification`)
-
-unique(statcan_grad_employ$`Classification Code for Field of study`)
-unique(statcan_grad_employ$`Hierarchy for Field of study`)
-
-unique(statcan_grad_employ$`Classification Code for Gender`) # NA
-unique(statcan_grad_employ$`Hierarchy for Gender`)
-
-unique(statcan_grad_employ$`Classification Code for Age group`) # NA
-unique(statcan_grad_employ$`Hierarchy for Age group`)
-
-unique(statcan_grad_employ$`Classification Code for Status of student in Canada`) # NA
-unique(statcan_grad_employ$`Hierarchy for Status of student in Canada`)
-
-unique(statcan_grad_employ$`Classification Code for Characteristics after graduation`) # NA
-unique(statcan_grad_employ$`Hierarchy for Characteristics after graduation`)
-
-unique(statcan_grad_employ$`Classification Code for Graduate statistics`) # NA
-unique(statcan_grad_employ$`Hierarchy for Graduate statistics`)
-
-length(unique(statcan_grad_employ$`val_norm`))
-statcan_grad_employ %>% filter(val_norm != VALUE) %>% nrow() #0
-
-unique(statcan_grad_employ$`Date`)
-# [1] "2010-07-01" "2011-07-01" "2012-07-01" "2013-07-01" "2014-07-01"
-# [6] "2015-07-01"
-
-unique(statcan_grad_employ$`Educational qualification`) # 32 levels
-unique(statcan_grad_employ$`Field of study`) # 60 levels
-unique(statcan_grad_employ$`Gender`)
-unique(statcan_grad_employ$`Age group`)
-unique(statcan_grad_employ$`Status of student in Canada`)
-unique(statcan_grad_employ$`Characteristics after graduation`)
-unique(statcan_grad_employ$`Graduate statistics`)
