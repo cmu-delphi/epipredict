e592d984b2a9548cf250b793899c984779c34a1d
Daniel J. McDonald <dajmcdon@gmail.com>
2022-12-27 10:26:53 -0800

    all tests pass, flag removed

--- R/layer_add_target_date.R
+++ R/layer_add_target_date.R
@@ -45,8 +45,8 @@
 #' p2
 layer_add_target_date <-
   function(frosting, target_date = NULL, id = rand_id("add_target_date")) {
+    target_date <- arg_to_date(target_date, allow_null = TRUE)
     arg_is_chr_scalar(id)
-    arg_is_date(target_date)
     add_layer(
       frosting,
       layer_add_target_date_new(
