886f77c11def0e584d596f6d156c0bd0de621dc7
Daniel J. McDonald <dajmcdon@gmail.com>
2023-01-06 12:49:16 -0800

    tests for parsnip mode validator

---
+++ tests/testthat/test-parsnip_model_validation.R
@@ -0,0 +1,19 @@
+test_that("forecaster can validate parsnip model", {
+  l <- list()
+  trainer1 <- parsnip::linear_reg()
+  trainer2 <- parsnip::logistic_reg()
+  trainer3 <- parsnip::rand_forest()
+
+  expect_error(get_parsnip_mode(l))
+  expect_equal(get_parsnip_mode(trainer1), "regression")
+  expect_equal(get_parsnip_mode(trainer2), "classification")
+  expect_equal(get_parsnip_mode(trainer3), "unknown")
+
+  expect_error(is_classification(l))
+  expect_true(is_regression(trainer1))
+  expect_false(is_classification(trainer1))
+  expect_true(is_classification(trainer2))
+  expect_false(is_regression(trainer2))
+  expect_true(is_regression(trainer3))
+  expect_true(is_classification(trainer3))
+})
