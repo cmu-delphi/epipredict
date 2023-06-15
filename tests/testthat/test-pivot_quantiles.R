e7db92bd9c521473e93dbd5539d2c4eef3839de2
Daniel J. McDonald <dajmcdon@gmail.com>
2023-06-15 09:21:43 -0700

    checks pass

--- tests/testthat/test-pivot_quantiles.R
+++ tests/testthat/test-pivot_quantiles.R
@@ -1,5 +1,5 @@
 test_that("quantile pivotting behaves", {
-  tib <- tibble(a = 1:5, b = 6:10)
+  tib <- tibble::tibble(a = 1:5, b = 6:10)
   expect_error(pivot_quantiles(tib, a))
   tib$c <- rep(dist_normal(), 5)
   expect_error(pivot_quantiles(tib, c))
@@ -17,7 +17,7 @@ test_that("quantile pivotting behaves", {
 
   d1 <- c(dist_quantiles(1:3, 1:3 / 4), dist_quantiles(2:4, 1:3 / 4))
   d2 <- c(dist_quantiles(2:4, 2:4 / 5), dist_quantiles(3:5, 2:4 / 5))
-  tib <- tibble(g = c("a", "b"), d1 = d1, d2 = d2)
+  tib <- tibble::tibble(g = c("a", "b"), d1 = d1, d2 = d2)
 
 
   expect_length(pivot_quantiles(tib, c("d1", "d2")), 7L)
