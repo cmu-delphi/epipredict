69f0c12e73f4b8c21c4ddac91ec73c36a8f0295f
rachlobay <rachel.lobay@gmail.com>
2023-07-03 13:29:34 -0700

    Modified get_test_data fun to omit excess rows when lag != 0

--- tests/testthat/test-get_test_data.R
+++ tests/testthat/test-get_test_data.R
@@ -26,6 +26,7 @@ test_that("expect insufficient training data error", {
   expect_error(get_test_data(recipe = r, x = case_death_rate_subset))
 })
 
+
 test_that("expect error that geo_value or time_value does not exist", {
   r <-  epi_recipe(case_death_rate_subset) %>%
     step_epi_ahead(death_rate, ahead = 7) %>%
@@ -76,4 +77,23 @@ test_that("NA fill behaves as desired", {
  expect_true(!any(is.na(td2)))
 
 
+})
+
+test_that("Omit end rows according to minimum lag when that’s not lag 0", {
+  ca <- case_death_rate_subset %>%
+    filter(geo_value == "ca")
+
+  rec <- epi_recipe(ca) %>%
+    step_epi_lag(case_rate, lag = c(2, 4, 6)) %>%
+    step_epi_ahead(case_rate, ahead = 7) %>%
+    step_epi_naomit()
+
+  td <- get_test_data(rec, ca)
+
+  td_res <- bake(prep(rec, ca), td)
+  td_row1to5_res <-  bake(prep(rec, ca), td[1:5, ])
+
+  expect_equal(td_res, td_row1to5_res)
+  expect_equal(nrow(td_res), 1L)
+  expect_equal(td_res$time_value, as.Date("2021-12-31"))
 })
