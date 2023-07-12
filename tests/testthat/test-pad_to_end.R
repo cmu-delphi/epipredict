82c7712ec22fc84c9c478e81a3a808991a088f33
Daniel J. McDonald <dajmcdon@gmail.com>
2023-07-12 11:42:57 -0700

    padding test complete

--- tests/testthat/test-pad_to_end.R
+++ tests/testthat/test-pad_to_end.R
@@ -3,7 +3,7 @@ test_that("test set padding works", {
     gr1 = rep(c("a", "b"), times = c(3, 4)),
     time_value = c(1:3, 1:4),
     value = 1:7
-  )
+  ) %>% arrange(time_value, gr1)
   expect_identical(pad_to_end(dat, "gr1", 3), dat)
   expect_equal(nrow(pad_to_end(dat, "gr1", 4)), 8L)
   p <- pad_to_end(dat, "gr1", 5)
@@ -12,4 +12,20 @@ test_that("test set padding works", {
   expect_identical(p$time_value, rep(1:5, each = 2))
   expect_identical(p$value, as.integer(c(1, 4, 2, 5, 3, 6, NA, 7, NA, NA)))
 
+  dat <- dat %>% arrange(gr1)
+  dat$gr2 <- c("c", "c", "d", "c", "c", "d", "d")
+  dat <- dat %>% arrange(time_value)
+  # don't treat it as a group
+  p <- pad_to_end(dat, "gr1", 4)
+  expect_identical(nrow(p), 8L)
+  expect_identical(p$gr2, c(rep("c", 4), "d", "d", NA, "d"))
+
+  # treat it as a group (needs different time_value)
+  dat$time_value <- c(1, 1, 2, 2, 1, 1, 2) # double
+  p <- pad_to_end(dat, c("gr1", "gr2"), 2)
+  expect_equal(nrow(p), 8L)
+  expect_identical(p$gr1, rep(c("a", "a", "b", "b"), times = 2))
+  expect_identical(p$gr2, rep(c("c", "d"), times = 4))
+  expect_identical(p$time_value, rep(c(1, 2), each = 4))
+  expect_identical(p$value, as.integer(c(1, 3, 4, 6, 2, NA, 5, 7)))
 })
