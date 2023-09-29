16139fffa1335efe250f350d5d6b6b020061cc64
Daniel J. McDonald <dajmcdon@gmail.com>
2023-09-29 16:21:37 -0700

    add lifecycle, deprecate pivot_quantiles.

--- R/epipredict-package.R
+++ R/epipredict-package.R
@@ -1,5 +1,8 @@
+## usethis namespace: start
 #' @importFrom tibble tibble
 #' @importFrom rlang := !!
 #' @importFrom stats poly predict lm residuals quantile
+#' @importFrom lifecycle deprecated
 #' @import epiprocess parsnip
+## usethis namespace: end
 NULL
