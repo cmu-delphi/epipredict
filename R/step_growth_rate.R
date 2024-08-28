aba9fa481445b01fb79ea51fddcf964f2650beaa
Daniel J. McDonald <dajmcdon@gmail.com>
2024-08-27 17:55:38 -0700

    redocument, remove browser()

--- R/step_growth_rate.R
+++ R/step_growth_rate.R
@@ -190,7 +190,6 @@ bake.step_growth_rate <- function(object, new_data, ...) {
 
 
   if (!is.null(object$replace_Inf)) {
-    browser()
     gr <- gr %>%
       mutate(across(!all_of(ok), ~ vec_replace_inf(.x, object$replace_Inf)))
   }
