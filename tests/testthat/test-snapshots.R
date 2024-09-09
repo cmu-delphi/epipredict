71694d717b9e8936f81cc2e76662ae85088f2d04
dsweber2 <david.weber2@pm.me>
2024-09-09 17:23:07 -0500

    non-timezone dependent printing tests

--- tests/testthat/test-snapshots.R
+++ tests/testthat/test-snapshots.R
@@ -112,6 +112,7 @@ test_that("arx_forecaster snapshots", {
 test_that("arx_forecaster output format snapshots", {
   jhu <- case_death_rate_subset %>%
     dplyr::filter(time_value >= as.Date("2021-12-01"))
+  attributes(jhu)$metadata$as_of <- as.Date(attributes(jhu)$metadata$as_of)
   out1 <- arx_forecaster(
     jhu, "death_rate",
     c("case_rate", "death_rate")
