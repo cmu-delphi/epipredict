4d8a24dd45735ae77ba754af169ede46b0e41fa0
Daniel J. McDonald <dajmcdon@gmail.com>
2024-09-13 15:47:00 -0700

    import rlang checks since checkmate doesn't take an environment

--- R/epipredict-package.R
+++ R/epipredict-package.R
@@ -1,15 +1,20 @@
 ## usethis namespace: start
-#' @importFrom tibble as_tibble
-#' @importFrom rlang := !! %||% as_function global_env set_names !!!
-#'             is_logical is_true inject enquo enquos expr sym arg_match
-#' @importFrom stats poly predict lm residuals quantile
-#' @importFrom dplyr arrange across all_of any_of bind_cols bind_rows group_by
-#'             summarize filter mutate select left_join rename ungroup full_join
-#'             relocate summarise everything
-#' @importFrom cli cli_abort cli_warn
-#' @importFrom checkmate assert assert_character assert_int assert_scalar
-#'             assert_logical assert_numeric assert_number assert_integer
-#'             assert_integerish assert_date assert_function assert_class
 #' @import epiprocess parsnip
+#' @importFrom checkmate assert assert_character assert_int assert_scalar
+#' @importFrom checkmate assert_integerish assert_date assert_function
+#' @importFrom checkmate assert_class assert_logical assert_numeric assert_number
+#' @importFrom checkmate assert_integer
+#' @importFrom cli cli_abort cli_warn
+#' @importFrom dplyr relocate summarise summarize everything
+#' @importFrom dplyr filter mutate select left_join rename ungroup full_join
+#' @importFrom dplyr arrange across all_of any_of bind_cols bind_rows group_by
+#' @importFrom rlang is_logical is_true inject enquo enquos expr sym arg_match
+#' @importFrom rlang := !! %||% as_function global_env set_names !!!
+#' @importFrom rlang caller_arg is_missing is_string is_symbol is_call
+#' @importFrom rlang env_get_list is_environment is_function is_closure is_formula
+#' @importFrom rlang is_character is_vector is_list
+#' @importFrom stats poly predict lm residuals quantile
+#' @importFrom tibble as_tibble
+na_chr <- NA_character_
 ## usethis namespace: end
 NULL
