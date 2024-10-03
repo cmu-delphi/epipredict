65448b5969f4a7d8fc3c168041d8878d4c1aa513
Dmitry Shemetov <dshemetov@ucdavis.edu>
2024-10-02 19:17:46 -0700

    fix: simplify

--- data-raw/state_census.R
+++ data-raw/state_census.R
@@ -1,14 +1,10 @@
 library(dplyr)
 library(tidyr)
 
-remote_file <- "https://github.com/cmu-delphi/covidcast/raw/refs/heads/main/R-packages/covidcast/data/state_census.rda"
-download.file(remote_file, "state_census.rda")
-load("state_census.rda")
-state_census <- state_census %>%
+state_census <- read_csv("https://github.com/cmu-delphi/covidcast/raw/c89e4d295550ba1540d64d2cc991badf63ad04e5/Python-packages/covidcast-py/covidcast/geo_mappings/state_census.csv") %>%
   select(STATE, NAME, POPESTIMATE2019, ABBR) %>%
   rename(abbr = ABBR, name = NAME, pop = POPESTIMATE2019, fips = STATE) %>%
   mutate(abbr = tolower(abbr)) %>%
   as_tibble()
-file.remove("state_census.rda")
 
 usethis::use_data(state_census, overwrite = TRUE)
