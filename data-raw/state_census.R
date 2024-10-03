c86ac10fdef6323f7830d2bd9a4b6322f9cf45bd
Dmitry Shemetov <dshemetov@ucdavis.edu>
2024-10-02 18:57:16 -0700

    refactor: remove Suggests dependence on covidcast * just download the state data file, don't import covidcast * covidcast depends on sf which is tough to install

--- data-raw/state_census.R
+++ data-raw/state_census.R
@@ -1,11 +1,14 @@
 library(dplyr)
 library(tidyr)
 
-state_census <- covidcast::state_census %>%
+remote_file <- "https://github.com/cmu-delphi/covidcast/raw/refs/heads/main/R-packages/covidcast/data/state_census.rda"
+download.file(remote_file, "state_census.rda")
+load("state_census.rda")
+state_census <- state_census %>%
   select(STATE, NAME, POPESTIMATE2019, ABBR) %>%
   rename(abbr = ABBR, name = NAME, pop = POPESTIMATE2019, fips = STATE) %>%
   mutate(abbr = tolower(abbr)) %>%
   as_tibble()
-
+file.remove("state_census.rda")
 
 usethis::use_data(state_census, overwrite = TRUE)
