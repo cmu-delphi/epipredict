8b1e48297648592746924dcb092c300f60e39f47
Daniel J. McDonald <dajmcdon@gmail.com>
2024-10-04 12:14:14 -0700

    examples run, mostly untested

--- R/step_pivot_wider.R
+++ R/step_pivot_wider.R
@@ -2,23 +2,43 @@
 
 #' Create new variables by pivotting data
 #'
+#' This function typically creates new predictors by sharing values across keys.
+#' So in the most basic case (see examples below), the values of a signal in
+#' one `geo_value` would be used as predictors in all the other locations.
+#'
 #' @inheritParams step_growth_rate
-#' @param ... One or more selector functions to choose variables
+#' @param ... <[`tidy-select`][tidyr_tidy_select]> One or more selector
+#'  functions to choose variables
 #'  values to pivot. These are the `values_from` argument for [tidyr::pivot_wider()].
 #'  See [recipes::selections()] for more details.
+#' @param names_from A selector function to choose which column (or columns) to
+#'  get the name of the output columns from. This is typically `geo_value`
+#'  (the default), and possibly any additional keys in the training data.
+#' @param id_cols <[`tidy-select`][tidyr_tidy_select]> A selector function
+#'  providing a set of columns that uniquely identifies each observation.
+#'  The typical use is for this to be `time_value` and any additional keys
+#'  not selected by `names_from` (this is the default behaviour).
 #' @inheritParams tidyr::pivot_wider
 #'
 #' @template step-return
 #' @export
 #'
 #' @examples
-#' 1+1
+#' jhu <- case_death_rate_subset %>%
+#'   filter(geo_value %in% c("ca", "ny", "pa"), time_value > "2021-12-01")
+#' r <- epi_recipe(jhu)
+#'
+#' r1 <- r %>% step_pivot_wider("death_rate")
+#' bake(prep(r1, jhu), new_data = NULL)
+#'
+#' r2 <- r %>% step_pivot_wider(dplyr::ends_with("rate"))
+#' bake(prep(r2, jhu), new_data = NULL)
 step_pivot_wider <- function(
     recipe,
     ...,
+    names_from = "geo_value",
     role = "predictor",
-    names_from,
-    id_cols = NULL,
+    id_cols = "time_value",
     id_expand = FALSE,
     values_fill = NA,
     values_fn = NULL,
@@ -74,8 +94,13 @@ step_pivot_wider_new <- function(
 #' @export
 prep.step_pivot_wider <- function(x, training, info = NULL, ...) {
   user_id_cols <- recipes_eval_select(x$user_id_cols, training, info)
-  all_id_cols <- union(user_id_cols, key_colnames(training))
-  hardhat::validate_column_names(training, all_id_cols)
+  hardhat::validate_column_names(training, user_id_cols)
+  names_from <- recipes_eval_select(x$names_from, training, info)
+  remaining_ids <- setdiff(
+    union(user_id_cols, names_from), # keys from user
+    key_colnames(training)           # all edf keys
+  )
+  all_id_cols <- union(user_id_cols, remaining_ids)
   step_pivot_wider_new(
     terms = x$terms,
     role = x$role,
@@ -83,7 +108,7 @@ prep.step_pivot_wider <- function(x, training, info = NULL, ...) {
     user_id_cols = user_id_cols,
     edf_id_cols = all_id_cols,
     id_expand = x$id_expand,
-    names_from = recipes_eval_select(x$names_from, training, info),
+    names_from = names_from,
     values_fill = x$values_fill,
     values_fn = x$values_fn,
     values_from = recipes_eval_select(x$terms, training, info),
@@ -94,15 +119,19 @@ prep.step_pivot_wider <- function(x, training, info = NULL, ...) {
 
 #' @export
 bake.step_pivot_wider <- function(object, new_data, ...) {
-  hardhat::validate_column_names(new_data, object$all_id_cols)
-  id_cols <- union(object$all_id_cols, key_colnames(new_data))
-  object$edf_id_cols <- id_cols
+  id_cols <- object$edf_id_cols
+  names_from <- object$names_from
+  values_from <- object$values_from
+  browser()
+  hardhat::validate_column_names(new_data, id_cols)
+  hardhat::validate_column_names(new_data, names_from)
+  hardhat::validate_column_names(new_data, values_from)
   if (length(id_cols) == 0L) {
     pivotted <- tidyr::pivot_wider(
-      new_data,
+      new_data[, c(names_from, values_from)],
       id_expand = object$id_expand,
-      names_from = unname(object$names_from),
-      values_from = unname(object$values_from),
+      names_from = unname(names_from),
+      values_from = unname(values_from),
       values_fill = object$values_fill,
       values_fn = object$values_fn,
       names_repair = "unique"
@@ -110,11 +139,11 @@ bake.step_pivot_wider <- function(object, new_data, ...) {
     joinby <- intersect(names(pivotted), names(new_data))
   } else {
     pivotted <- tidyr::pivot_wider(
-      new_data,
+      new_data[, c(id_cols, names_from, values_from)],
       id_cols = unname(id_cols),
       id_expand = object$id_expand,
-      names_from = unname(object$names_from),
-      values_from = unname(object$values_from),
+      names_from = unname(names_from),
+      values_from = unname(values_from),
       values_fill = object$values_fill,
       values_fn = object$values_fn,
       names_repair = "unique"
