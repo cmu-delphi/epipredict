fef636cc1653bef8719451c6324254bb96b0185f
Daniel J. McDonald <dajmcdon@gmail.com>
2024-12-03 14:50:46 -0800

    finish climate predictor draft

--- R/step_climate_predictor.R
+++ R/step_climate_predictor.R
@@ -1,45 +1,39 @@
-#' Calculate a growth rate
+#' Calculate a climatological variable based on the history
 #'
-#' `step_growth_rate()` creates a *specification* of a recipe step
-#'   that will generate one or more new columns of derived data.
+#' `step_climate()` creates a *specification* of a recipe step
+#'   that will generate one or more new columns of derived data. Using this as
+#'   a predictor is most useful when predicting strongly seasonal data. The
+#'   climate timing will automatically be aligned to the outcome at bake time.
 #'
 #'
-#' @inheritParams step_epi_lag
-#' @param horizon Bandwidth for the sliding window, when `method` is
-#'   "rel_change" or "linear_reg". See [epiprocess::growth_rate()] for more
-#'   details.
-#' @param method Either "rel_change" or "linear_reg",
-#'   indicating the method to use for the growth rate
-#'   calculation. These are local methods: they are run in a sliding
-#'   fashion over the sequence (in order to estimate derivatives and hence
-#'   growth rates). See [epiprocess::growth_rate()] for more details.
-#' @param log_scale Should growth rates be estimated using the parameterization
-#'   on the log scale? See details for an explanation. Default is `FALSE`.
-#' @param replace_Inf Sometimes, the growth rate calculation can result in
-#'   infinite values (if the denominator is zero, for example). In this case,
-#'   most prediction methods will fail. This argument specifies potential
-#'   replacement values. The default (`NA`) will likely result in these rows
-#'   being removed from the data. Alternatively, you could specify arbitrary
-#'   large values, or perhaps zero. Setting this argument to `NULL` will result
-#'   in no replacement.
-#' @param additional_gr_args_list A list of additional arguments used by
-#'   [epiprocess::growth_rate()]. All `...` arguments may be passed here along
-#'   with `dup_rm` and `na_rm`.
+#' @inheritParams step_growth_rate
+#' @param time_type The duration over which time aggregation should be performed.
+#' @param center_method The measure of center to be calculated over the time
+#'   window.
+#' @param window_size Scalar integer. How many time units on each side should
+#'   be included. For example, if `window_size = 3` and `time_type = "day"`,
+#'   then on each day in the data, the center will be calculated using 3 days
+#'   before and three days after. So, in this case, it operates like a weekly
+#'   rolling average, centered at each day.
+#' @param epi_keys Character vector or `NULL`. Any columns mentioned will be
+#'   grouped before performing any center calculation. So for example, given
+#'   state-level data, a national climate would be calculated if `NULL`, but
+#'   passing `epi_keys = "geo_value"` would calculate the climate separately
+#'   by state.
 #' @template step-return
 #'
 #'
-#' @family row operation steps
-#' @importFrom epiprocess growth_rate
 #' @export
 #' @examples
 #' r <- epi_recipe(covid_case_death_rates) %>%
-#'   step_growth_rate(case_rate, death_rate)
+#'   step_epi_ahead(death_rate, ahead = 7) %>%
+#'   step_climate(death_rate, time_type = "day")
 #' r
 #'
 #' r %>%
 #'   prep(covid_case_death_rates) %>%
 #'   bake(new_data = NULL)
-step_climate_predictor <-
+step_climate <-
   function(recipe,
            ...,
            role = "predictor",
@@ -62,18 +56,18 @@ step_climate_predictor <-
     arg_is_chr_scalar(prefix, id)
     arg_is_lgl_scalar(skip)
 
-    aggr <- switch(time_type,
-                   epiweek = lubridate::epiweek, week = lubridate::week,
-                   month = lubridate::month, day = lubridate::yday)
+    time_aggr <- switch(time_type,
+                        epiweek = lubridate::epiweek, week = lubridate::week,
+                        month = lubridate::month, day = lubridate::yday)
 
     recipes::add_step(
       recipe,
-      step_climate_predictor_new(
+      step_climate_new(
         terms = enquos(...),
         role = role,
         trained = FALSE,
         time_type = time_type,
-        aggr = aggr,
+        time_aggr = time_aggr,
         center_method = center_method,
         window_size = window_size,
         epi_keys = epi_keys,
@@ -88,12 +82,12 @@ step_climate_predictor <-
   }
 
 
-step_climate_predictor_new <-
+step_climate_new <-
   function(terms,
            role,
            trained,
            time_type,
-           aggr,
+           time_aggr,
            center_method,
            window_size,
            epi_keys,
@@ -104,12 +98,12 @@ step_climate_predictor_new <-
            id,
            case_weights) {
     recipes::step(
-      subclass = "climate_predictor",
+      subclass = "climate",
       terms = terms,
       role = role,
       trained = trained,
       time_type = time_type,
-      aggr = aggr,
+      time_aggr = time_aggr,
       center_method = center_method,
       window_size = window_size,
       epi_keys = epi_keys,
@@ -125,7 +119,7 @@ step_climate_predictor_new <-
 
 
 #' @export
-prep.step_climate_predictor <- function(x, training, info = NULL, ...) {
+prep.step_climate <- function(x, training, info = NULL, ...) {
   col_names <- recipes_eval_select(x$terms, training, info)
   recipes::check_type(training[, col_names], types = c("double", "integer"))
   wts <- recipes::get_case_weights(info, training)
@@ -137,7 +131,7 @@ prep.step_climate_predictor <- function(x, training, info = NULL, ...) {
   modulus <- switch(x$time_type, epiweek = 7L, week = 7L, month = 12L, day = 365L)
 
   result <- training %>%
-    mutate(.idx = x$aggr(time_value), .weights = wts) %>%
+    mutate(.idx = x$time_aggr(time_value), .weights = wts) %>%
     select(.idx, .weights, c(col_names, x$epi_keys)) %>%
     summarize(across(
       all_of(col_names),
@@ -147,12 +141,12 @@ prep.step_climate_predictor <- function(x, training, info = NULL, ...) {
     ) %>%
     unnest(col_names)
 
-  step_climate_predictor_new(
+  step_climate_new(
     terms = x$terms,
     role = x$role,
     trained = TRUE,
     time_type = x$time_type,
-    aggr = x$aggr,
+    time_aggr = x$time_aggr,
     center_method = x$center_method,
     window_size = x$window_size,
     epi_keys = x$epi_keys,
@@ -168,16 +162,16 @@ prep.step_climate_predictor <- function(x, training, info = NULL, ...) {
 
 
 #' @export
-bake.step_climate_predictor <- function(object, new_data, ...) {
+bake.step_climate <- function(object, new_data, ...) {
   new_data %>%
-    mutate(.idx = object$aggr(.target_time_value)) %>%
+    mutate(.idx = object$time_aggr(.target_time_value)) %>%
     left_join(object$result, by = c(".idx", object$epi_keys)) %>%
     select(-.idx)
 }
 
 
 #' @export
-print.step_climate_predictor <- function(x, width = max(20, options()$width - 30), ...) {
+print.step_climate <- function(x, width = max(20, options()$width - 30), ...) {
   print_epi_step(
     x$columns, x$terms, x$trained,
     title = "Calculating climate_predictor for ",
