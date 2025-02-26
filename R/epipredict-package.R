f411be67c4c0f20bd6e150d93add8e4ff31748a5
Daniel J. McDonald <dajmcdon@gmail.com>
2025-02-26 11:54:55 -0800

    imports from epiprocess

--- R/epipredict-package.R
+++ R/epipredict-package.R
@@ -9,11 +9,11 @@
 #' @importFrom dplyr full_join relocate summarise everything
 #' @importFrom dplyr inner_join
 #' @importFrom dplyr summarize filter mutate select left_join rename ungroup
+#' @importFrom epiprocess growth_rate growth_rate_params is_epi_df
 #' @importFrom magrittr extract2
 #' @importFrom rlang := !! %||% as_function global_env set_names !!! caller_arg
 #' @importFrom rlang is_logical is_true inject enquo enquos expr sym arg_match
 #' @importFrom stats poly predict lm residuals quantile
 #' @importFrom tibble as_tibble
-na_chr <- NA_character_
 ## usethis namespace: end
 NULL
