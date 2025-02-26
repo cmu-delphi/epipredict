b90b9be6ceed77485a9ed06ecd49ab0e9eafdc75
Daniel J. McDonald <dajmcdon@gmail.com>
2025-02-26 11:51:39 -0800

    adjust reexports to include epiprocess necessaries, also frequently used dplyr, tibble, tidyr

---
+++ R/reexports.R
@@ -0,0 +1,59 @@
+#' @importFrom dplyr filter
+#' @export
+dplyr::filter
+
+#' @importFrom dplyr mutate
+#' @export
+dplyr::mutate
+
+#' @importFrom dplyr rename
+#' @export
+dplyr::rename
+
+#' @importFrom dplyr select
+#' @export
+dplyr::select
+
+#' @importFrom epiprocess as_epi_df
+#' @export
+epiprocess::as_epi_df
+
+#' @importFrom epiprocess key_colnames
+#' @export
+epiprocess::key_colnames
+
+#' @importFrom generics fit
+#' @export
+generics::fit
+
+#' @importFrom generics forecast
+#' @export
+generics::forecast
+
+#' @importFrom generics tidy
+#' @export
+generics::tidy
+
+#' @importFrom recipes prep
+#' @export
+recipes::prep
+
+#' @importFrom recipes bake
+#' @export
+recipes::bake
+
+#' @importFrom recipes rand_id
+#' @export
+recipes::rand_id
+
+#' @importFrom tibble as_tibble
+#' @export
+tibble::as_tibble
+
+#' @importFrom tibble tibble
+#' @export
+tibble::tibble
+
+#' @importFrom tidyr unnest
+#' @export
+tidyr::unnest
