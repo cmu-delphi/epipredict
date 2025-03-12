6c5e70f6779c339609a8b4c8953961faf73d40fe
dsweber2 <david.weber2@pm.me>
2025-03-12 16:18:39 -0500

    updating for climatological

--- R/layer_threshold_preds.R
+++ R/layer_threshold_preds.R
@@ -61,7 +61,11 @@ layer_threshold_new <-
 
 
 
-# restrict various objects to the interval [lower, upper]
+#' restrict various objects to the interval [lower, upper]
+#' @param x the object to restrict
+#' @param lower numeric, the lower bound
+#' @param upper numeric, the upper bound
+#' @param ... unused
 #' @export
 #' @keywords internal
 snap <- function(x, lower, upper, ...) {
